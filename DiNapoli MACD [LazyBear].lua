e2c96e74c67cf6a421f0789378462b28c5ca6059
Karejmuskwe <muskwec@gmail.com>
2024-11-24 17:12:18 -0600

    Update DiNapoli MACD [LazyBear].lua

--- DiNapoli MACD [LazyBear].lua
+++ DiNapoli MACD [LazyBear].lua
@@ -8,8 +8,8 @@ lc = 17.5185
 sc = 8.3896
 sp = 9.0503
 src = close
-fs = nz(fs[1]) + 2.0 / (1.0 + sc) * (src- nz(fs[1]))
-ss = nz(ss[1]) + 2.0 / (1.0 + lc) * (src - nz(ss[1]))
+fs = nz(fs[1]) + 2.0 / (1.2 + sc) * (src- nz(fs[1]))
+ss = nz(ss[1]) + 2.0 / (1.2 + lc) * (src - nz(ss[1]))
 r = fs - ss
 s = nz(s[1]) + 2.0/(1 + sp)*(r - nz(s[1]))
 
@@ -21,5 +21,5 @@ rect {
     first = 0,
     second = r,
     color = iff(r>0 ,'green','red'),
-    width = 0.4
+    width = 0.2
 }
